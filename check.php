e52798d0c1432fa3ea3ac2c6ad64fdb78b3b4649
arturterbaghdasarov1997 <133400776+arturterbaghdasarov1997@users.noreply.github.com>
2024-02-03 18:43:07 +0400

    Update check.php

--- check.php
+++ check.php
@@ -8,21 +8,27 @@
         switch($operator){
             case "+":
                 $result = $num1 + $num2;
+                echo ($result);
                 break;
             case "-":
                 $result = $num1 - $num2;
+                echo ($result);
                 break;
             case "*":
                 $result = $num1 * $num2;
+                echo ($result);
                 break;
             case "/":
                 if ($num2 != 0) {
                     $result = $num1 / $num2;
+                    echo ($result);
                 } else {
                     $result = "Division by zero is not allowed";
+                    echo ("Division by zero is not allowed");
                 }
                 break;
             default:
                 $result = "Invalid operator";
+                echo ("Invalid operator");
         }
     }
